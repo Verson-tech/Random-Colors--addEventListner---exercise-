5217f7b183193a5b6e2f1618215b7697c52a6b24
vira.murray <vira.avramenko@gmail.com>
2022-01-06 17:07:12 +0200

    h1.style.color = "white";

--- app.js
+++ app.js
@@ -1,16 +1,19 @@
 const button = document.querySelector('button');
 const h1 = document.querySelector('h1');
-
-button.addEventListener('click', function () {
-    const newColor = makeRandColor();
+button.addEventListener('click', function(){
+    const newColor = randomColor();
     document.body.style.backgroundColor = newColor;
-    h1.innerText = newColor;
-})
-
-const makeRandColor = () => {
+    h1.innerText= newColor;
+});
+const randomColor = () => {
     const r = Math.floor(Math.random() * 255);
     const g = Math.floor(Math.random() * 255);
     const b = Math.floor(Math.random() * 255);
-    return `rgb(${r}, ${g}, ${b})`;
+    if (r < 80 && g < 80 && b < 80 ){
+        h1.style.color = "white";
+    }
+    else{
+        h1.style.color="black";
+    }
+return `rgb(${r}, ${g}, ${b})`;
 }
-
